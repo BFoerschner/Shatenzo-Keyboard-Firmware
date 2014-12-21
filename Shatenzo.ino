6f759780a46fd68144a427aa937f9f2254e3b8b7
Bjoern Foerschner <sirius.dwayne@t-online.de>
2014-12-21 18:20:48 +0100

    Recognizing Individual Keypresses
    - Printing them into Serialbus

--- Shatenzo.ino
+++ Shatenzo.ino
@@ -1,20 +1,39 @@
 // Shatenzo.ino
 
-const byte NUMBER_OF_ROWS = 2;
-const byte NUMBER_OF_COLUMNS = 1;
+const byte NUMBER_OF_ROWS = 5;
+const byte NUMBER_OF_COLUMNS = 3;
 
-byte rowPins[NUMBER_OF_ROWS] = {0, 1};
-byte columnPins[NUMBER_OF_COLUMNS] = {5};
+byte rowPins[NUMBER_OF_ROWS] = {0,1,2,3,4};
+byte columnPins[NUMBER_OF_COLUMNS] = {5,6,7};
 
-int keyValues[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] = {
-    {KEY_A},
-    {KEY_B}
-};
+void checkForKeypresses(    
+    const byte rowPins[],
+    const byte rowPinsSize, 
+    const byte columnPins[], 
+    const byte columnPinsSize
+) {
+    int i = 0;
+    int k = 0;
 
-boolean keyPressed[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] = {
-    {false},
-    {false}
-};
+    for (i = 0; i < rowPinsSize; ++i) {
+        pinMode(rowPins[i], HIGH);
+
+        for (k = 0; k < columnPinsSize; ++k) {
+            boolean keyDown = (digitalRead(columnPins[k]) == 0);
+            
+            if (keyDown) {
+                pinMode(rowPins[i], LOW);
+
+                boolean exactKeyDown = (digitalRead(columnPins[k]) == 0);
+                if (exactKeyDown) {
+                    Serial.print(i); Serial.print(":"); Serial.println(k);
+                }
+            }
+        }
+
+        pinMode(rowPins[i], LOW);
+    }
+}
 
 void initKeys (
     const byte rowPins[],
@@ -24,19 +43,21 @@ void initKeys (
 ) {
     int i = 0;
 
-    for (i = 0;, i < rowPinsSize; ++i) {
+    for (i = 0; i < rowPinsSize; ++i) {
         pinMode(rowPins[i], OUTPUT);
     }
 
-    for (i = 0;, i < columnPinsSize; ++i) {
-        pinMode(columnPins[i], INPUT);
+    for (i = 0; i < columnPinsSize; ++i) {
+        pinMode(columnPins[i], INPUT_PULLUP);
     }
 }
 
 void setup() {
-	Serial.begin(9600);
+    Serial.begin(9600);
+    initKeys(rowPins, NUMBER_OF_ROWS, columnPins, NUMBER_OF_COLUMNS);
 }
 
 void loop() {
+    checkForKeypresses(rowPins, NUMBER_OF_ROWS, columnPins, NUMBER_OF_COLUMNS);
+    delay(1500);
 }
-

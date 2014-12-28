d67fbc7184278e3c47d7791c3068ced4ce4e9a33
Bjoern Foerschner <sirius.dwayne@t-online.de>
2014-12-28 15:05:44 +0100

    KeyDown and KeyUp Events work now
    - Fixed a Bug where it didn't register multiple Keypresses in a row

--- Shatenzo.ino
+++ Shatenzo.ino
@@ -1,10 +1,25 @@
 // Shatenzo.ino
 
 const byte NUMBER_OF_ROWS = 5;
-const byte NUMBER_OF_COLUMNS = 3;
+const byte NUMBER_OF_COLUMNS = 14;
 
-byte rowPins[NUMBER_OF_ROWS] = {0,1,2,3,4};
-byte columnPins[NUMBER_OF_COLUMNS] = {5,6,7};
+byte rowPins[NUMBER_OF_ROWS] = {0, 1, 2, 3, 4};
+byte columnPins[NUMBER_OF_COLUMNS] = {21, 20, 19, 18, 17, 16, 15, 14, 12, 11, 10, 9, 8, 7};
+byte lastKnownState[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+byte currentState[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS] = {
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
 
 void checkForKeypresses(    
     const byte rowPins[],
@@ -16,23 +31,73 @@ void checkForKeypresses(
     int k = 0;
 
     for (i = 0; i < rowPinsSize; ++i) {
-        pinMode(rowPins[i], HIGH);
-
         for (k = 0; k < columnPinsSize; ++k) {
-            boolean keyDown = (digitalRead(columnPins[k]) == 0);
-            
-            if (keyDown) {
+            currentState[i][k] = 0;
+            pinMode(rowPins[i], HIGH);
+
+            boolean columnPressed = digitalRead(columnPins[k]) == 0;
+            if (columnPressed) {
                 pinMode(rowPins[i], LOW);
 
-                boolean exactKeyDown = (digitalRead(columnPins[k]) == 0);
-                if (exactKeyDown) {
-                    Serial.print(i); Serial.print(":"); Serial.println(k);
+                boolean currentKeyDown = (digitalRead(columnPins[k]) == 0);
+                if (currentKeyDown) {
+                    // Serial.print(rowPins[i]); Serial.print(":"); Serial.println(columnPins[k]);
+                    currentState[i][k] = 1;
                 }
+                pinMode(rowPins[i], HIGH);
             }
+            pinMode(rowPins[i], LOW);
         }
-
         pinMode(rowPins[i], LOW);
     }
+    handleStateChange();
+    debounce();
+    // debugKeyboard();
+}
+
+void debounce() {
+    delay(10);
+}
+
+void handleStateChange() {
+    int i;
+    int k;
+
+    for (i = 0; i < NUMBER_OF_ROWS; ++i) {
+        for (k = 0; k < NUMBER_OF_COLUMNS; ++k) {
+            if (currentState[i][k] != lastKnownState[i][k]) {
+                if (currentState[i][k] == 1) {
+                    keyDown(i, k);
+                } else {
+                    keyUp(i, k);
+                }
+            }
+            lastKnownState[i][k] = currentState[i][k];
+            currentState[i][k] = 0;
+        }
+    }
+}
+
+void keyUp(const int i, const int k) {
+    Serial.print("Key "); Serial.print(i); Serial.print(":"); Serial.print(k); Serial.println(" up");
+}
+
+void keyDown(const int i, const int k) {
+    Serial.print("Key "); Serial.print(i); Serial.print(":"); Serial.print(k); Serial.println(" down");
+}
+
+void debugKeyboard() {
+    int i=0;
+    int k=0;
+
+    for (i = 0; i < NUMBER_OF_ROWS; ++i) {
+        for (k = 0; k < NUMBER_OF_COLUMNS; ++k) {
+            Serial.print(currentState[i][k]); Serial.print(" ");
+        }
+        Serial.println("");
+    }
+    Serial.println("");
+    Serial.println("--------------------------------------------------------");
 }
 
 void initKeys (
@@ -59,5 +124,4 @@ void setup() {
 
 void loop() {
     checkForKeypresses(rowPins, NUMBER_OF_ROWS, columnPins, NUMBER_OF_COLUMNS);
-    delay(1500);
 }
